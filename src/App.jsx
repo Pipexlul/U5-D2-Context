670ab0df0082e81b0c1be2e1e6302e36b50bca1e
Pipexlul <felipgg@gmail.com>
2023-03-05 19:07:15 -0300

    Start adding React Router functionality and create boilerplate for all the views

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,40 @@
+import { createHashRouter, RouterProvider, Navigate } from "react-router-dom";
+
+// Views
+import Root from "./views/Root";
+import Home from "./views/Home";
+import Browse from "./views/Browse";
+import Favorites from "./views/Favorites";
+import ErrorPage from "./views/ErrorPage";
+
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <Navigate to="/home" replace={true} />,
+      },
+      {
+        path: "/home",
+        element: <Home />,
+      },
+      {
+        path: "/browse",
+        element: <Browse />,
+      },
+      {
+        path: "/favorites",
+        element: <Favorites />,
+      },
+    ],
+  },
+]);
+
 function App() {
-  return null;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/views/Root.jsx
@@ -0,0 +1,7 @@
+import { Outlet } from "react-router-dom";
+
+const Root = () => {
+  return <div className="flex flex-col min-h-screen bg-gray-900"></div>;
+};
+
+export default Root;
