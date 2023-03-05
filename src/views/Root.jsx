f12b699d2aded736dbb36cc10f86cab8ed617aab
Pipexlul <felipgg@gmail.com>
2023-03-05 20:01:29 -0300

    Implement Navbar component and add boilerplate for Footer Component

--- src/views/Root.jsx
+++ src/views/Root.jsx
@@ -3,6 +3,9 @@ import { Outlet } from "react-router-dom";
 
 import AppContext from "../contexts/appContext";
 
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
 const Root = () => {
   const reducerFunc = (state, action) => {
     switch (action.type) {
@@ -22,10 +25,31 @@ const Root = () => {
 
   const [favorites, favoritesDispatch] = useReducer(reducerFunc, []);
 
+  const linkData = [
+    {
+      label: "Home",
+      path: "/home",
+    },
+    {
+      label: "Buscar",
+      path: "/browse",
+    },
+    {
+      label: "Favoritos",
+      path: "/favorites",
+    },
+  ];
+
   return (
     <div className="flex flex-col min-h-screen bg-gray-900">
       <AppContext.Provider value={{ favorites, favoritesDispatch }}>
-        <Navbar />
+        <Navbar
+          header="Forest Collection"
+          headerGradient="bg-gradient-to-r from-emerald-300 via-yellow-300 to-emerald-300"
+          bgLinkColor="bg-fuchsia-600"
+          textLinkColor="text-fuchsia-400"
+          navLinks={linkData}
+        />
         <Outlet />
         <Footer />
       </AppContext.Provider>
