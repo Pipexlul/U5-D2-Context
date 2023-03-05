7157806260b6746fd2e8772fcdb71e6362f3f11f
Pipexlul <felipgg@gmail.com>
2023-03-05 19:28:51 -0300

    Start completing Root view. It makes use of useReducer instead of useState. Enclose app in the AppContext context.

--- src/views/Root.jsx
+++ src/views/Root.jsx
@@ -1,7 +1,36 @@
+import { useState, useReducer } from "react";
 import { Outlet } from "react-router-dom";
 
+import AppContext from "../contexts/appContext";
+
 const Root = () => {
-  return <div className="flex flex-col min-h-screen bg-gray-900"></div>;
+  const reducerFunc = (state, action) => {
+    switch (action.type) {
+      case "ADD_FAVORITE":
+        return [...state, action.payload];
+
+      case "REMOVE_FAVORITE":
+        return state.filter((fav) => fav.id !== action.payload);
+
+      case "RESET_FAVORITES":
+        return [];
+
+      default:
+        return state;
+    }
+  };
+
+  const [favorites, favoritesDispatch] = useReducer(reducerFunc, []);
+
+  return (
+    <div className="flex flex-col min-h-screen bg-gray-900">
+      <AppContext.Provider value={{ favorites, favoritesDispatch }}>
+        <Navbar />
+        <Outlet />
+        <Footer />
+      </AppContext.Provider>
+    </div>
+  );
 };
 
 export default Root;
